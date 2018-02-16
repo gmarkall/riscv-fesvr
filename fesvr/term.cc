318eda7537865d0ae374af17cb3bedc04760596a
Graham Markall <graham.markall@embecosm.com>
2018-02-16 15:25:00 +0000

    Bodge to kill all output. Prev attempt didnt work but this is good enough for creduce

--- fesvr/term.cc
+++ fesvr/term.cc
@@ -7,7 +7,7 @@
 
 bool foreground = true;
 
-void ttou_handler(int signal)
+extern "C" void ttou_handler(int signal)
 {
   foreground = false;
 }
@@ -20,13 +20,13 @@ class canonical_termios_t
   {
     signal(SIGTTOU, ttou_handler);
 
-    if (tcgetattr(0, &old_tios) == 0)
-    {
-      struct termios new_tios = old_tios;
-      new_tios.c_lflag &= ~(ICANON | ECHO);
-      if (tcsetattr(0, TCSANOW, &new_tios) == 0)
-        restore_tios = true;
-    }
+    //if (tcgetattr(0, &old_tios) == 0)
+    //{
+    //  struct termios new_tios = old_tios;
+    //  new_tios.c_lflag &= ~(ICANON | ECHO);
+    //  if (tcsetattr(0, TCSANOW, &new_tios) == 0)
+    //    restore_tios = true;
+    //}
   }
 
   ~canonical_termios_t()
@@ -44,25 +44,26 @@ static canonical_termios_t tios; // exit() will clean up for us
 
 int canonical_terminal_t::read()
 {
-  if (foreground)
-  {
-    struct pollfd pfd;
-    pfd.fd = 0;
-    pfd.events = POLLIN;
-    int ret = poll(&pfd, 1, 0);
-    if (ret <= 0 || !(pfd.revents & POLLIN))
-      return -1;
-
-    unsigned char ch;
-    ret = ::read(0, &ch, 1);
-    return ret <= 0 ? -1 : ch;
-  }
-  else
+//  if (foreground)
+//  {
+//    struct pollfd pfd;
+//    pfd.fd = 0;
+//    pfd.events = POLLIN;
+//    int ret = poll(&pfd, 1, 0);
+//    if (ret <= 0 || !(pfd.revents & POLLIN))
+//      return -1;
+//
+//    unsigned char ch;
+//    ret = ::read(0, &ch, 1);
+//    return ret <= 0 ? -1 : ch;
+//  }
+//  else
     return -1;
 }
 
 void canonical_terminal_t::write(char ch)
 {
-  if (foreground && (::write(1, &ch, 1) != 1))
-    abort();
+  //if (foreground)
+  //  if (::write(1, &ch, 1) != 1)
+  //    abort();
 }
